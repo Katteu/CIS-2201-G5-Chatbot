6935c597cc6a2b14a27dc7cb6f52658fdf0ce277
PonPonC <gybaez321@gmail.com>
2023-05-02 00:20:02 +0800

    [Update] Added Fron-End design for Customize section

--- src/modules/Chatbot/customize.tsx
+++ src/modules/Chatbot/customize.tsx
@@ -8,7 +8,45 @@ const Customize = ({openC,onCloseC}:{openC:boolean,onCloseC:MouseEventHandler<HT
               <div className='topTitle'>
                 <h1 className='sectionTitle'>Customize</h1>
                   <p onClick={onCloseC} className='closeBtn'>X</p>
-              </div>  
+              </div> 
+              <div className='themeHeader'>
+                <h1 className='customTitle'>Hello Daddy UWU</h1>
+              </div> 
+              <div className='parentSect'>
+                <div className='leftHalf'>
+                  <div className='themeRow'>
+                    <div className='defaultTheme'>
+                    <i className="fa-solid fa-pen-to-square"></i>
+                    </div>
+                  </div>
+                  <div className='themeRow'>
+                    <div className='cobaltTheme'>
+                    </div>
+                  </div>
+                  <div className='themeRow'>
+                    <div className='royaltyTheme'>
+                    </div>
+                  </div>
+                </div>
+                <div className='rightHalf'>
+                  <div className='themeRow'>
+                    <div className='forestmossTheme'>
+                    </div>
+                  </div>
+                  <div className='themeRow'>
+                    <div className='soyeclipseTheme'>
+                    </div>
+                  </div>
+                  <div className='themeRow'>
+                    <div className='fiestaredTheme'>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='footerExit'>
+              <p onClick={onCloseC} className='cancelBtn'>Cancel</p>
+                <p onClick={onCloseC} className='saveBtn'>Save</p>
+              </div>
           </div>
       </div>
     )
